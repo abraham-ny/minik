1b3059a2ba34b4a8538725274c74c3fb63892bd1
Abraham .M <abummoja3@gmail.com>
2025-04-04 23:52:21 +0300

    Minor changes

--- firebase-snippet.js
+++ firebase-snippet.js
@@ -6,15 +6,7 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBkDdJetfyAwQqsYVKp5a8hGxC1fzQfSkA",
-  authDomain: "minik-2486f.firebaseapp.com",
-  projectId: "minik-2486f",
-  storageBucket: "minik-2486f.firebasestorage.app",
-  messagingSenderId: "507980853656",
-  appId: "1:507980853656:web:7df4cf83dec459979c3bed",
-  measurementId: "G-T6YDSQWTSX"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
